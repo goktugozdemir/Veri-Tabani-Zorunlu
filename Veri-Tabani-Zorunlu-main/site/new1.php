b3d8d7dcb42bcc130c903a2d5d2c56556782a61b
Ahmet Göktuğ Özdemir <56187372+goktugozdemir@users.noreply.github.com>
2021-05-29 20:15:55 +0300

    Add files via upload

---
+++ Veri-Tabani-Zorunlu-main/site/new1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kütüphane</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+
+
+<style>
+body,h1 {font-family: Arial, sans-serif}
+h1 {letter-spacing: 6px}
+
+body {
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url('');
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+</style>
+<body>
+
+
+<div style="text-align:center" style="max-width:1500px" ">
+
+<header style="padding:128px 16px">
+  <h1>Bursa</h1>
+  <h1>Kütüphaneleri</h1>
+  
+  
+  <form action="asd.php" method="POST">  
+            <label>Kütüphane Numaranız: </label>   
+            <input type="ad" id="ad" name="ad" >  
+            <label>Şifreniz : </label>   
+            <input type="sifre" id="sifre "name="sifre">  
+            <button type="giriş">Giriş</button>       
+    </form>     
+	
+      <p style="position: absolute; bottom: 30px;">İletişim Bilgileri</p>
+	  <p style="position: absolute; bottom: 5px;">Telefon Numarası:555555555555   E-posta adresi:123@hotmail.com</p>
+
+</header>
+
+</div>
+
+
+</body>
+</html>
